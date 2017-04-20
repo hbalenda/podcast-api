fb672bfd3da1856723c1672aaf0478bff9eb43c8
hcbviolet <hviolet@thestranger.com>
2017-04-19 19:16:43 -0700

    Stub routes

--- server.js
+++ server.js
@@ -1,118 +1,45 @@
-// BASE SETUP
+const env        = require('./.env');
+const express    = require('express');
+const app        = express();
+const request    = require('request');
+const Feed       = require('rss-to-json');
+const contentful = require('contentful')
+
+const port = process.env.PORT || 8080; // set our port
+
+// ROUTES
 // =============================================================================
 
-// call the packages we need
-var express    = require('express');
-var bodyParser = require('body-parser');
-var app        = express();
-var morgan     = require('morgan');
+const router = express.Router();
 
-// configure app
-app.use(morgan('dev')); // log requests to the console
-
-// configure body parser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-var port     = process.env.PORT || 8080; // set our port
-
-var mongoose   = require('mongoose');
-mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o'); // connect to our database
-var Bear     = require('./app/models/bear');
-
-// ROUTES FOR OUR API
-// =============================================================================
-
-// create our router
-var router = express.Router();
-
-// middleware to use for all requests
 router.use(function(req, res, next) {
-	// do logging
 	console.log('Something is happening.');
 	next();
 });
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-	res.json({ message: 'hooray! welcome to our api!' });	
+// /RSS
+router.get('/rss', function(req, res) {
+	Feed.load('http://feeds.soundcloud.com/users/soundcloud:users:203577071/sounds.rss', function(err, rss){
+	   res.json(rss);
+	})
 });
 
-// on routes that end in /bears
-// ----------------------------------------------------
-router.route('/bears')
-
-	// create a bear (accessed at POST http://localhost:8080/bears)
-	.post(function(req, res) {
-		
-		var bear = new Bear();		// create a new instance of the Bear model
-		bear.name = req.body.name;  // set the bears name (comes from the request)
-
-		bear.save(function(err) {
-			if (err)
-				res.send(err);
-
-			res.json({ message: 'Bear created!' });
-		});
-
-		
+// /ABOUT
+router.get('/about', function(req, res) {
+	const client = contentful.createClient({
+	  space: ENV.SPACE_ID,
+	  accessToken: ENV.ACCESS_TOKEN
 	})
+	client.getEntry(ENV.ABOUT_ENTRY_ID)
+	  .then((response) => {
+	  	res.json(response.fields)
+	  })
+	  .catch((error) => {
+	  	res.json(error)
+	  })
+});
 
-	// get all the bears (accessed at GET http://localhost:8080/api/bears)
-	.get(function(req, res) {
-		Bear.find(function(err, bears) {
-			if (err)
-				res.send(err);
-
-			res.json(bears);
-		});
-	});
-
-// on routes that end in /bears/:bear_id
-// ----------------------------------------------------
-router.route('/bears/:bear_id')
-
-	// get the bear with that id
-	.get(function(req, res) {
-		Bear.findById(req.params.bear_id, function(err, bear) {
-			if (err)
-				res.send(err);
-			res.json(bear);
-		});
-	})
-
-	// update the bear with this id
-	.put(function(req, res) {
-		Bear.findById(req.params.bear_id, function(err, bear) {
-
-			if (err)
-				res.send(err);
-
-			bear.name = req.body.name;
-			bear.save(function(err) {
-				if (err)
-					res.send(err);
-
-				res.json({ message: 'Bear updated!' });
-			});
-
-		});
-	})
-
-	// delete the bear with this id
-	.delete(function(req, res) {
-		Bear.remove({
-			_id: req.params.bear_id
-		}, function(err, bear) {
-			if (err)
-				res.send(err);
-
-			res.json({ message: 'Successfully deleted' });
-		});
-	});
-
-
-// REGISTER OUR ROUTES -------------------------------
+// REGISTER ROUTES ON /API
 app.use('/api', router);
 
 // START THE SERVER
