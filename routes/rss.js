4276b98c7cac72ed7082606ffc87ba0fee6e2b47
hcbviolet <hviolet@thestranger.com>
2017-06-03 17:50:26 -0700

    Change rss feed

--- routes/rss.js
+++ routes/rss.js
@@ -1,7 +1,7 @@
 const Feed = require('rss-to-json');
 
 module.exports = function(req, res) {
-  Feed.load('http://feeds.soundcloud.com/users/soundcloud:users:203577071/sounds.rss', function(err, rss){
+  Feed.load("http://feeds.soundcloud.com/users/soundcloud:users:301028661/sounds.rss", function(err, rss){
      res.json(rss);
   })
 };
