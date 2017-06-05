b99f92b01aadbaae21c733beff667c9d44d1ea6f
hcbviolet <hviolet@thestranger.com>
2017-06-04 21:48:23 -0700

    Parse linebreaks

--- routes/rss.js
+++ routes/rss.js
@@ -1,7 +1,22 @@
-const Feed = require('rss-to-json');
+var FeedMe = require('feedme');
+var http = require('http');
 
 module.exports = function(req, res) {
-  Feed.load("http://feeds.soundcloud.com/users/soundcloud:users:301028661/sounds.rss", function(err, rss){
-     res.json(rss);
-  })
+  http.get('http://feeds.soundcloud.com/users/soundcloud:users:301028661/sounds.rss', function(response) {
+    var parser = new FeedMe(true);
+    const rss = { items: [] };
+    parser.on('item', function(item) {
+      parsedDescription = item.description.replace(/(?:\r\n|\r|\n)/g, '\n\n');
+      rss.items.push({
+        'title': item.title,
+        'description': parsedDescription,
+        'link': item.link,
+        'enclosure': item.enclosure
+      });
+    });
+    response.pipe(parser);
+    parser.on('end', function() {
+      res.json(rss);
+    });
+  });
 };
